4a9e355b84c5dd55157ad1e089e12d5a43e5a411
kknaresh <naresh.k@cngl.ie>
2015-07-22 22:31:33 +0100

    Create and Show Form in String

--- src/main/java/com/springmvc/sample/controllers/RootController.java
+++ src/main/java/com/springmvc/sample/controllers/RootController.java
@@ -3,43 +3,58 @@ package com.springmvc.sample.controllers;
 import javax.annotation.Resource;
 import javax.mail.MessagingException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springmvc.sample.dto.SignupForm;
 import com.springmvc.sample.mail.MailSender;
 import com.springmvc.sample.mail.MockMailSender;
 
 @Controller
 public class RootController {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(RootController.class);
+
 	private MailSender mailSender;
-	
+
 	@Autowired
 	public RootController(MailSender mailSender) {
 		this.mailSender = mailSender;
 	}
-	
-//	@RequestMapping("/")
-//	public String home() throws MessagingException {
-//		
-//		//mailSender.send("abc@example.com", "Hello, World", "Mail from spring");		
-//		return "home";
-//		
-//	}
-	
-	@RequestMapping("/signup")
+
+	// @RequestMapping("/")
+	// public String home() throws MessagingException {
+	//
+	// //mailSender.send("abc@example.com", "Hello, World", "Mail from spring");
+	// return "home";
+	//
+	// }
+
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signup(Model model) {
-		model.addAttribute("name","Naresh");
-		
-		return "signup"; //View Name
-		
+
+		model.addAttribute(new SignupForm());
+
+		return "signup";
+
 	}
-	
-	
+
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String signup(@ModelAttribute("signupForm") SignupForm signupForm) {
+
+		logger.info(signupForm.toString());
+
+		return "redirect:/";
+
+	}
 
 }
--- src/main/webapp/WEB-INF/jsp/includes/header.jsp
+++ src/main/webapp/WEB-INF/jsp/includes/header.jsp
@@ -2,6 +2,7 @@
 	pageEncoding="utf-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
 <!DOCTYPE html>
 <html lang="en">
 <head>
